b0bd52f78018e5bd1bb4b27d1dad7ee060a0a8ac
bhushanblaster <55149243+bhushanblaster@users.noreply.github.com>
2021-04-07 20:41:37 +0530

    Update Detect_Loop_in_linked_List.cpp

--- Section 29 : Linked List/Detect_Loop_in_linked_List.cpp
+++ Section 29 : Linked List/Detect_Loop_in_linked_List.cpp
@@ -1,3 +1,4 @@
+//Given a linked list of N nodes. The task is to check if the linked list has a loop. Linked list can contain self loop.
 bool detectLoop(Node* head)
     {
        struct Node* slow = head,*fast = head;
